904445fb24f1c7b194baa1e652ca123a8a179204
samtheteddybear <sbpiyal@gmail.com>
2018-02-25 01:49:53 -0500

    Add files via upload

---
+++ 02.01.18.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using std::string; 
+using namespace std; 
+
+/*
+int main(){
+	int x;
+	cout<<"Enter a number: ";
+	cin>>x;
+	cout<<"Your number is "<<x<<endl;
+
+	cout<<"the size of interger x is "<<sizeof(x)<<endl; 
+	
+	
+	double d=100000000000000000.0;
+	cout<<"size of double is "<<sizeof(d)<<endl;
+	
+	/*
+	
+	Note: Because double does not have arbitary preicion some algebraic laws will to work! 
+	Ex: if d+a=d+e, then a should be equal to e 
+	
+	*/
+	
+	//double a=0.0; //(if you leave the decimal then it wil be an integer, if with decimal then it will be a floating point)
+//	double e = 0.00000001; 
+	
+	// 0.0 and 0.000001 will be the same if you use the data type double because at some point it loses the data as it goes along the way
+//	if (d+a==d+e){
+//		cout<<"e was equal to a"<<endl;
+//	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	return 0;
+ /* *This gives a value back to the shell* (for VM use echo '$?') 
+	* Convention 0 means "everything is ok"
+    * and any mother value means "something went wrong" 
+*/
+	
+/* 
+FOR VM- 
+* ./a.out && echo " it worked :D"
+ to see if it gives you the vaue back 0 
+*/	
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
